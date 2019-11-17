b4a7766a6674d485368c338bf301b1ddac422034
AKash WaDhawane <akash3699@gmail.com>
2019-11-17 16:55:07 +0530

    reverted changes and added mysql 5.7 version

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM 200.0.1.1:5000/mysql
+FROM mysql:5.7
 
 ENV MYSQL_ROOT_PASSWORD=root
 ENV MYSQL_DATABASE=app_db
 
 COPY schema.sql /docker-entrypoint-initdb.d/
+
+# CMD service mysql restart
 
 EXPOSE 3306
--- schema.sql
+++ schema.sql
@@ -1,16 +1,18 @@
 create table category (id integer primary key auto_increment, title varchar(100));
 
--- insert default categories
+
 insert into category(title) values ('Electronics');
 insert into category(title) values ('Fashion');
 insert into category(title) values ('Home');
 insert into category(title) values ('Kitchen');
 insert into category(title) values ('TV');
 
--- create the product table
+
 create table product (id integer primary key auto_increment, title varchar(100), description varchar(500), price float, category_id integer);
 
--- create the user table
+
 create table user (id integer primary key auto_increment, name varchar(100), email varchar(100), password varchar(100), phone varchar(20));
 
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+-- use mysql;
+-- update user set authentication_string=password(''), plugin='mysql_native_password' where user='root';
